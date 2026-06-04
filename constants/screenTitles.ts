3f1340bdf4811b4e04c2b61c72576f556ced15c4
Hassaan_98 <hassaanxd8@gmail.com>
2026-06-04 12:20:21 +0500

    Update screenTitles.ts

--- constants/screenTitles.ts
+++ constants/screenTitles.ts
@@ -1,5 +1,6 @@
 export const NESTED_SCREEN_TITLES: Record<string, string> = {
   "/qrCodeHistory": "QR Code History",
   "/qrCodeDetails/": "QR Code Details",
-  '/basicInformation':'Basic Information'
+  '/basicInformation':'Basic Information',
+  "/telemetry": "Telemetry",
 };
