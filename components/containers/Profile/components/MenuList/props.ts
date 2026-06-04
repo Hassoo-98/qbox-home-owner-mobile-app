5ad8fd8e87108b16981bbc0a07b1e9e32f9ded9b
Hassaan_98 <hassaanxd8@gmail.com>
2026-06-04 12:16:13 +0500

    Update props.ts

--- components/containers/Profile/components/MenuList/props.ts
+++ components/containers/Profile/components/MenuList/props.ts
@@ -2,5 +2,6 @@ import { ProfileItem } from "@/types";
 
  export interface MenuListProps {
     menuData: ProfileItem[];
+    onItemPress?: (item: ProfileItem) => boolean | Promise<boolean>;
   }
   
