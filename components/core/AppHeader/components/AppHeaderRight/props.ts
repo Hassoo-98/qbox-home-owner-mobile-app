81368fb695b79a4a4a82b0530536742538e5b034
Hassaan_98 <hassaanxd8@gmail.com>
2026-06-08 18:18:06 +0500

    Update props.ts

--- components/core/AppHeader/components/AppHeaderRight/props.ts
+++ components/core/AppHeader/components/AppHeaderRight/props.ts
@@ -2,6 +2,7 @@ export interface AppHeaderRightProps {
   activeTab: string;
   handleQRPress: () => void;
   handleNotificationPress: () => void;
+  notificationCount?: number;
   handleRefreshPress?: () => void;
   isRefreshLoading?: boolean;
 }
